b4604ba72db56ef43a5440dfae8a91b8bfbc668d
Maxim Mitin <maximus405@gmail.com>
2021-11-04 00:22:33 +0300

    Initial Commit

---
+++ Avacados/AvacadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvacadosApp.swift
+//  Avacados
+//
+//  Created by Maxim Mitin on 4.11.21.
+//
+
+import SwiftUI
+
+@main
+struct AvacadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
