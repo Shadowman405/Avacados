151286990744afbae5d60ac7bfa4093cad22c6a9
Maxim Mitin <maximus405@gmail.com>
2021-11-04 01:25:34 +0300

    init

---
+++ Avacados/View/RipeningStagesView.swift
@@ -0,0 +1,20 @@
+//
+//  RipeningStagesView.swift
+//  Avacados
+//
+//  Created by Maxim Mitin on 4.11.21.
+//
+
+import SwiftUI
+
+struct RipeningStagesView: View {
+    var body: some View {
+        Text("Ripening Stages")
+    }
+}
+
+struct RipeningStagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        RipeningStagesView()
+    }
+}
---
+++ Avacados/View/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  Avacados
+//
+//  Created by Maxim Mitin on 4.11.21.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
